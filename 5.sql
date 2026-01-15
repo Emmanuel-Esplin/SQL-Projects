b75166f28f42cd1059584f268d45b09f33f13f43
Emmanuel-Esplin <163627755+Emmanuel-Esplin@users.noreply.github.com>
2026-01-15 21:48:51 +0200

    Delete 5.sql

--- 5.sql
+++
@@ -1,7 +0,0 @@
-""" Retrieve distinct category names from categories that have products listed in the Products table """
-SELECT DISTINCT category_name
-FROM categories
-WHERE category_id IN (
-    SELECT category_id FROM Products
-)
-ORDER BY category_name;
